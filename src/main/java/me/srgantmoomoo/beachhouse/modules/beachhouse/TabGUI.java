26cfa2eab488a57833f5a4eacecc06c1382c1df1
moomooooo <69589624+moomooooo@users.noreply.github.com>
2021-07-03 23:11:53 -0400

    tabgui start

--- src/main/java/me/srgantmoomoo/beachhouse/modules/beachhouse/TabGUI.java
+++ src/main/java/me/srgantmoomoo/beachhouse/modules/beachhouse/TabGUI.java
@@ -1,11 +1,55 @@
 package me.srgantmoomoo.beachhouse.modules.beachhouse;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import me.srgantmoomoo.beachhouse.Main;
+import me.srgantmoomoo.bedroom.api.event.events.EventDrawOverlay;
 import me.srgantmoomoo.bedroom.module.Module;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.hud.InGameHud;
 
 public class TabGUI extends Module {
 	
 	public TabGUI() {
 		super("tab gui", "tabguiiiii.", 0, Category.BEACHHOUSE);
 	}
+	
+	@Override
+	public void onEnable() {
+		Main.EVENTBUS.subscribe(listener);
+		expanded = true;
+	}
+	
+	@Override
+	public void onDisable() {
+		Main.EVENTBUS.unsubscribe(listener);
+	}
+	
+	int categoryIndex = 0;
+	int moduleIndex = 0;
+	boolean expanded;
+	List<Module> currentModules = new ArrayList<>();
+	
+	TextRenderer tr = MinecraftClient.getInstance().textRenderer;
 
+	@EventHandler
+	private final Listener<EventDrawOverlay> listener = new Listener<>(e -> {
+		// categories
+		InGameHud.fill(e.matrix, 0, 12, 70, 82, 0x80000000);
+		InGameHud.fill(e.matrix, 0, (categoryIndex * 14) + 12, 70, (categoryIndex * 14) + 26, 0xff000000);
+		int yOffset = 14;
+		for(Category c : Category.values()) {
+			if(expanded && Category.values()[categoryIndex] == c) {
+				tr.drawWithShadow(e.matrix, c.name + " <", 2, yOffset + 1, 0xffffffff);
+			}else {
+				tr.drawWithShadow(e.matrix, c.name + " >", 2, yOffset + 1, 0xffffffff);
+			}
+			yOffset += 14;
+		}
+	});
+	
 }
--- src/main/java/me/srgantmoomoo/bedroom/ui/UI.java
+++ src/main/java/me/srgantmoomoo/bedroom/ui/UI.java
@@ -1,11 +1,7 @@
 package me.srgantmoomoo.bedroom.ui;
 
-import java.util.Comparator;
-
 import me.srgantmoomoo.beachhouse.Main;
 import me.srgantmoomoo.bedroom.api.event.events.EventDrawOverlay;
-import me.srgantmoomoo.bedroom.module.Module;
-import me.srgantmoomoo.bedroom.module.ModuleManager;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import net.minecraft.client.MinecraftClient;
@@ -27,17 +23,6 @@ public class UI {
 	private final Listener<EventDrawOverlay> listener = new Listener<>(e -> {
 		TextRenderer tr = mc.textRenderer;
 		tr.drawWithShadow(e.matrix, Main.name + " " + Main.version, 2, 2, 0xffffffff);
-		
-		int y = 1;
-		final int[] counter = { 1 };
-		for (Module module : ModuleManager.getModules()) {
-			if (module.isEnabled()) {
-					tr.drawWithShadow(e.matrix, module.getName(), 2, 10 + y, 0x80808080);
-				y += tr.fontHeight;
-				counter[0]++;
-			}
-		}
-		ModuleManager.modules.sort(Comparator.comparing(module -> -mc.textRenderer.getWidth(module.getName())));
 	});
 
 }
