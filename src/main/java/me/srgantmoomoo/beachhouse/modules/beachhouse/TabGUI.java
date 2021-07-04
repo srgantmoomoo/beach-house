4d3b8ae1786973618edb5fcd185ae5c6df601b0e
moomooooo <69589624+moomooooo@users.noreply.github.com>
2021-07-04 16:06:27 -0400

    tabgui fucked up visuals but properly working now.

--- src/main/java/me/srgantmoomoo/beachhouse/modules/beachhouse/TabGUI.java
+++ src/main/java/me/srgantmoomoo/beachhouse/modules/beachhouse/TabGUI.java
@@ -29,7 +29,6 @@ public class TabGUI extends Module {
 	public void onEnable() {
 		Main.EVENTBUS.subscribe(overlayListener);
 		Main.EVENTBUS.subscribe(keyListener);
-		expanded = true;
 	}
 	
 	@Override
@@ -37,110 +36,92 @@ public class TabGUI extends Module {
 		Main.EVENTBUS.unsubscribe(overlayListener);
 		Main.EVENTBUS.unsubscribe(keyListener);
 	}
-	
-	int categoryIndex = 0;
-	int moduleIndex = 0;
-	boolean expanded;
-	List<Module> currentModules = ModuleManager.getModulesByCategory(Category.values()[categoryIndex]);;
-	
-	TextRenderer tr = MinecraftClient.getInstance().textRenderer;
+
+	public int currentTab;
+	public boolean expanded;
+	public boolean Tab;
 
 	@EventHandler
 	private final Listener<EventDrawOverlay> overlayListener = new Listener<>(e -> {
-		// categories
-		int boxColor = 0x80E6AB17;
-		if(theme.is("stealth")) boxColor = 0x80000000;
-		int selectorColor = 0xffF730FB;
-		if(theme.is("stealth")) selectorColor = 0xffffffff;
+		TextRenderer tr = MinecraftClient.getInstance().textRenderer;
 
-		InGameHud.fill(e.matrix, 1, 12, 66, 85, boxColor);
-		InGameHud.fill(e.matrix, 2, (categoryIndex * 12) + 13, 65, (categoryIndex * 12) + 24, selectorColor);
+		InGameHud.fill(e.matrix, 2, 12, 53, 71, 0x80000000);
+		InGameHud.fill(e.matrix, 3, 14 + currentTab * 15 - 1, 52, 14 + currentTab * 15 + 11, 0xff000000); //0x9993d3d3
 
 		int count = 0;
-		for(Category c : Category.values()) {
-			String categoryName = c.name;
-			if(c.name.equals("miscellaneous")) categoryName = "misc";
-			if(c.name.equals("beachhouse")) categoryName = "beach";
-
-			int catL = 1;
-			if(categoryName.equals("player")) catL = 17;
-			if(categoryName.equals("render")) catL = 16;
-			if(categoryName.equals("combat")) catL = 16;
-			if(categoryName.equals("movement")) catL = 10;
-			if(categoryName.equals("misc")) catL = 22;
-			if(categoryName.equals("beach")) catL = 17;
-
-			InGameHud.drawStringWithShadow(e.matrix, MinecraftClient.getInstance().textRenderer, categoryName, catL, 14 + count * 12, 0xffffffff);
+		for (Category c : Module.Category.values()) {
+			tr.drawWithShadow(e.matrix, c.name, 4, 4 + count * 14, 0xffffffff);
 			count++;
 		}
 
-		// modules
 		if (expanded) {
-			InGameHud.fill(e.matrix, 70, 12, 140, (currentModules.size() * 14) + 12, 0x90000000);
-			InGameHud.fill(e.matrix, 70, (moduleIndex * 14) + 12, 140, (moduleIndex * 14) + 26, 0xFF00A9A9);
-			int _yOffset = 14;
-			for (Module m : currentModules) {
-				TextFormatting color;
+			Category category = Module.Category.values()[currentTab];
+			List<Module> modules = ModuleManager.getModulesByCategory(category);
 
-				if (m.isEnabled())
-					color = TextFormatting.WHITE;
-				else
-					color = TextFormatting.GRAY;
+			if (modules.size() == 0)
+				return;
 
-				tr.drawWithShadow(e.matrix, color + m.getName(), 70 + 2, _yOffset + 1, 0xffffffff);
-				_yOffset += 14;
+			InGameHud.fill(e.matrix, 55, 12, 130, 11 + modules.size() * 15, 0x80000000);
+			InGameHud.fill(e.matrix, 56, 14 + category.moduleIndex * 15 - 1, 129, 14 + category.moduleIndex * 15 + 11, 0xff000000);
+
+			count = 0;
+			for (Module m : modules) {
+				tr.drawWithShadow(e.matrix, m.name, 4 + 53, 18 + count * 14, -1);
+				count++;
 			}
 		}
-
 	});
 
 	@EventHandler
 	private final Listener<EventKeyPress> keyListener = new Listener<>(e -> {
-		if (e.getKey() == GLFW.GLFW_KEY_UP) {
-			if (!expanded) {
-				if (categoryIndex == 0)
-					categoryIndex = Category.values().length - 1;
-				else
-					categoryIndex--;
-			} else {
-				if (moduleIndex == 0)
-					moduleIndex = currentModules.size() - 1;
-				else
-					moduleIndex--;
+
+		int code = ((EventKeyPress)e).getKey();
+
+		Category category = Module.Category.values()[currentTab];
+		List<Module> modules = ModuleManager.getModulesByCategory(category);
+
+		if(code == GLFW.GLFW_KEY_UP) {
+			if(expanded) {
+				if(category.moduleIndex <= 0) {
+					category.moduleIndex = modules.size() - 1;
+				}else
+					category.moduleIndex--;
+			}else {
+				if(currentTab <= 0) {
+					currentTab = Module.Category.values().length - 1;
+				}else
+					currentTab--;
 			}
 		}
 
-		if (e.getKey() == GLFW.GLFW_KEY_DOWN) {
-			if (!expanded) {
-				if (categoryIndex == Category.values().length - 1)
-					categoryIndex = 0;
-				else
-					categoryIndex++;
-			} else {
-				if (moduleIndex == currentModules.size() - 1)
-					moduleIndex = 0;
-				else
-					moduleIndex++;
-			}
-		}
-
-		if (e.getKey() == GLFW.GLFW_KEY_RIGHT) {
-			if (!expanded) {
-				expanded = true;
-				moduleIndex = 0;
-			} else {
-				currentModules.get(moduleIndex).toggle();
-			}
-		}
-
-		if (e.getKey() == GLFW.GLFW_KEY_LEFT) {
-			expanded = false;
-		}
-
-		if (e.getKey() == GLFW.GLFW_KEY_ENTER) {
+		if(code == GLFW.GLFW_KEY_DOWN) {
 			if (expanded) {
-				currentModules.get(moduleIndex).toggle();
+				if(category.moduleIndex >= modules.size() - 1) {
+					category.moduleIndex = 0;
+				}else
+					category.moduleIndex++;
+			}else {
+				if(currentTab >= Module.Category.values().length - 1) {
+					currentTab = 0;
+				}else
+					currentTab++;
 			}
+		}
+
+		if(code == GLFW.GLFW_KEY_RIGHT) {
+			if(expanded && modules.size() !=0) {
+				Module module = modules.get(category.moduleIndex);
+				if(!module.name.equals("TabGUI"))
+					module.toggle();
+			}else {
+				expanded = true;
+
+
+			}
+		}
+
+		if(code == GLFW.GLFW_KEY_LEFT) {
+			expanded = false;
 		}
 	});
 
