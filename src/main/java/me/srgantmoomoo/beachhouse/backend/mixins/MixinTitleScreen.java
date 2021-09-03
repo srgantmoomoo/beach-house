0773ac2e3a9e51cca10f2a18c2ba311a3af78b2e
moomooooo <69589624+moomooooo@users.noreply.github.com>
2021-09-03 17:53:27 -0400

    some title screen stuff. no splash screen stuff.

---
+++ src/main/java/me/srgantmoomoo/beachhouse/backend/mixins/MixinTitleScreen.java
@@ -0,0 +1,27 @@
+package me.srgantmoomoo.beachhouse.backend.mixins;
+
+import me.srgantmoomoo.beachhouse.backend.util.Reference;
+import net.minecraft.client.render.Camera;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.screen.*;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+
+@Mixin(TitleScreen.class)
+public class MixinTitleScreen extends Screen {
+
+    protected MixinTitleScreen(Text title) {
+        super(title);
+    }
+
+    @Inject(at = @At("RETURN"), method = "render")
+    public void render(MatrixStack matrix, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        int width = Reference.minecraft.getWindow().getScaledWidth();
+        int height = Reference.minecraft.getWindow().getScaledHeight();
+
+        drawStringWithShadow(matrix, this.textRenderer, "beach house is loaded !", width - 120, height - 20, 0xfff868fB );
+    }
+}
